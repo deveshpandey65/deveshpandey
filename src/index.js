3009d66604760b0d75159724abfb4e18d685cffd
Devesh <dp.devesh65@gmail.com>
2025-02-18 16:24:14 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -11,15 +11,14 @@ import Contact from './Elements/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/projectdesc/:projectName" element={<ProjectDesc />} />
-        <Route path='/contact' element={<Contact/>} />
-        
-      </Routes>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projectdesc/:projectName" element={<ProjectDesc />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
