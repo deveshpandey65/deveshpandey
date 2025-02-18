58031a5e91d9273548f417aa7fdeae78e670e58a
Devesh <dp.devesh65@gmail.com>
2025-02-18 16:31:25 +0530

    commit

--- src/Elements/Nav.jsx
+++ src/Elements/Nav.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assests/img/Untitled design.PNG';
 import './index.css';
+
 export default function Nav() {
   return (
     <div className="nav">
-        <div className="logo">
-            <img src={logo} alt="" />
-        </div>
-        <div className="nav-links">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-            <a href="/projects">Projects</a>
-            <a href="#services">Coding Profiles</a>
-            
-        </div>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/projects">Projects</Link>
+        <a href="#services">Coding Profiles</a>
+      </div>
     </div>
-  )
+  );
 }
-
--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ import About from './Elements/About';
 import Projects from './Elements/Projects';
 import ProjectDesc from './Elements/ProjectDesc';
 import Contact from './Elements/Contact';
+import Nav from './Components/Nav'; // Adjust the path as needed
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projectdesc/:projectName" element={<ProjectDesc />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projectdesc/:projectName" element={<ProjectDesc />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
